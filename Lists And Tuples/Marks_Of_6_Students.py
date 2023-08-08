a2691d4ec05a7e9e4c17c4c4173ff09913a96954
Muhammad Harris Rashid <125113774+commando839@users.noreply.github.com>
2023-08-08 22:14:41 +0500

    Add files via upload

---
+++ Lists And Tuples/Marks_Of_6_Students.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the marks of student no. 1: ")) 
+b = int(input("Enter the marks of student no. 2: "))
+c = int(input("Enter the marks of student no. 3: "))
+d = int(input("Enter the marks of student no. 4: "))
+e = int(input("Enter the marks of student no. 5: "))
+f = int(input("Enter the marks of student no. 6: "))
+
+marks = [a, b, c, d, e, f]
+marks.sort() 
+
+print (marks) 
